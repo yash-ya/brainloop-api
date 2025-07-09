768c564e74246a5d19636756061ef76c15a67491
Yash <yashraj25.yr@gmail.com>
2025-07-09 10:51:07 +0530

    Add Trusted Proxies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,6 +14,10 @@ func init() {
 
 func main() {
 	router := gin.Default()
+	err := router.SetTrustedProxies([]string{"127.0.0.1", "::1", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"})
+	if err != nil {
+		log.Fatal("Failed to set trusted proxies: ", err)
+	}
 	routes.SetupRoutes(router)
 
 	log.Println("Server running on port", config.AppConfig.Port)
