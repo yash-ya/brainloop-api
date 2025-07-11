ecc839ddce3e041f5d9911cfceb2fd6afd33c745
Yash <yashraj25.yr@gmail.com>
2025-07-11 18:26:54 +0530

    Add CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"brainloop-api/pkg/routes"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,6 +25,12 @@ func main() {
 	log.Println("Database migrated successfully.")
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		// We allow our local frontend to make requests
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	}))
 	err := router.SetTrustedProxies([]string{"127.0.0.1", "::1", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"})
 	if err != nil {
 		log.Fatal("Failed to set trusted proxies: ", err)
